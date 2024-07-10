c9815a9dbd51ce6fe9e3643cbfd45dcbfe968695
Carmen <monterocarmeng@gmail.com>
2024-07-10 16:37:07 -0400

    testing git

---
+++ Anti-Fraud System/task/src/antifraud/AntiFraudApplication.java
@@ -0,0 +1,12 @@
+package antifraud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntiFraudApplication {
+    //to push to git
+    public static void main(String[] args) {
+        SpringApplication.run(AntiFraudApplication.class, args);
+    }
+}
