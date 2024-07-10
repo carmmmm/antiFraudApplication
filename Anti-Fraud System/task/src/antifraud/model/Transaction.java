c676a7436f341ea60df0b20ebe23ff2755a3ab7b
Carmen <monterocarmeng@gmail.com>
2024-07-10 16:54:28 -0400

    simple transaction validation

---
+++ Anti-Fraud System/task/src/antifraud/model/Transaction.java
@@ -0,0 +1,14 @@
+package antifraud.model;
+
+public class Transaction {
+    private Long amount;
+
+    // Getters and Setters
+    public Long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Long amount) {
+        this.amount = amount;
+    }
+}
