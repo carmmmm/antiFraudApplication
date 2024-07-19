f802be514befa5a3773acc80b6a55b6fbfc9f508
Carmen <monterocarmeng@gmail.com>
2024-07-19 10:00:05 -0400

    fixed h2 console issue, but the GET and DELETE methods need authentication to work.

--- Anti-Fraud System/task/src/antifraud/config/SecurityConfig.java
+++ Anti-Fraud System/task/src/antifraud/config/SecurityConfig.java
@@ -1,14 +1,21 @@
 package antifraud.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.http.HttpMethod;
+
+import javax.management.relation.Role;
 
 
 //Configures Spring Security.
@@ -36,13 +43,27 @@ public class SecurityConfig {
                 .and()
                 .authorizeRequests()
                 .requestMatchers(HttpMethod.POST, "/api/auth/user").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/auth/list").permitAll()
+                .requestMatchers(HttpMethod.GET, "/api/auth/list").authenticated()
                 .requestMatchers("/actuator/shutdown").permitAll()
                 .requestMatchers("/h2-console/**").permitAll()
+                .requestMatchers(PathRequest.toH2Console()).permitAll()
+                .requestMatchers("/error").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
                 .and()
                 .build();
+    }
+
+    @Bean
+//    @Override
+    protected UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
     }
 }
